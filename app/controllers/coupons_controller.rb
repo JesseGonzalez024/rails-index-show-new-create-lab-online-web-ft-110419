bead22b29fa60cbb2ce4d143849788d23a07443c
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-28 21:48:28 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,22 @@
+class CouponsController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def show
+  
+  end
+  
+  def new 
+    
+  end 
+  
+  def create
+  
+  end 
+  
+  
+  
+  
+end
