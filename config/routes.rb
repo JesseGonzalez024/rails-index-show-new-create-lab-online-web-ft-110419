3d7924f5a710482bf7d4c3c9735ddc1144425ed6
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-28 22:03:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   resources :coupons, only: [:index, :new]
-  get '/coupon/:id', to: 'coupons#show', as: 'student'
+  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
   post '/coupon', to: 'coupons#create'
  
 end
